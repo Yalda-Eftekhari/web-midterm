e0e273b4e71277a147473d624e5d3b761669906b
Yalda <yalda.ef@gmail.com>
2023-01-06 17:59:46 +0330

    alerting and some improvements

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ const full_name = document.querySelector('.full_name');
 const blog = document.querySelector('.blog_address');
 const location_ = document.querySelector('.location');
 const bio = document.querySelector('.bio');
+const alertresult = document.querySelector('.alert_result');
 
 
 async function getInfo(e) {
@@ -16,22 +17,47 @@ async function getInfo(e) {
         let response = await fetch(`https://api.github.com/users/` + username);
         let obj = await response.json();
         if (response.status != 200) {
+            if(response.status == 404)
+                alerting("User not found")
+            else if(response.status == 403)
+                alerting("API rate limit exceeded")
+            else
+                alerting("Error")
             return Promise.reject(`Request failed with error ${response.status}`);
         }
         getUserInfo(obj);
-        let data = await JSON.parse(window.localStorage.getItem(username));
-        console.log(data);
     } catch (e) {
-        console.log(e);
+        alerting("Invalid input!");
     }
 }
 
 function getUserInfo(obj) {
     avatar.innerHTML = '<img src="' + obj.avatar_url + '" alt="avatar" class="avatar">';
-    full_name.innerHTML = '<span>' + obj.name + '</span>';
-    blog.innerHTML = '<span>' + obj.blog + '</span>';
-    location_.innerHTML = '<span>' + obj.location + '</span>';
-    bio.innerHTML = '<span>' + obj.bio + '</span>';
+    if (obj.name == null)
+        full_name.innerHTML = '<span>' + "name: " + '</span>';
+    else
+        full_name.innerHTML = '<span>' + "name: " + obj.name + '</span>';
+    if (obj.blog == null)
+        blog.innerHTML = '<span>' + "blog: " + '</span>';
+    else
+        blog.innerHTML = '<span>' + "blog: " + obj.blog + '</span>';
+    if (obj.location == null)
+        location_.innerHTML = '<span>' + "location: " + '</span>';
+    else
+        location_.innerHTML = '<span>' + "location: " + obj.location + '</span>';
+    if (obj.bio == null)
+        bio.innerHTML = '<span>' + "bio: " + '</span>';
+    else
+        bio.innerHTML = '<span>' + "bio: " + obj.bio + '</span>';
+}
+
+
+function alerting(title) {
+    alertresult.style.display = "block";
+    alertresult.innerHTML = "<span>" + title + "</span>";
+    setTimeout(() => {
+        alertresult.style.display = "none";
+    }, 3500);
 }
 
 submitButton.addEventListener('click', getInfo);
